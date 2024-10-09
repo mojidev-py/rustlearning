3065400f93a980ec6a45eb10e18f08fd0827816f
mojidev-py <pycharmdudeig@gmail.com>
2024-10-08 21:29:10 -0700

    if else statements

---
+++ proj5/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let num = 123;
+
+    if num > 100 {
+        println!("num is greater than 100 frr");
+    } else {
+        println!("num is less than or equal to 100 frrrr")
+    }
+
+}
